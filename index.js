8dc3c5bd2b5db15e4e94f86acf6a272a9564d5b6
Dev093235 <dev431140@gmail.com>
2025-08-14 22:28:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.get('/', function (req, res) {
 
 // Start the server and add error handling
 app.listen(port, () => {
-    logger(`Server is running on port ${port}...`, "[ Rudra ]");
+    logger(`Server is running on port ${port}...`, "[ MirryKal ]");
 }).on('error', (err) => {
     if (err.code === 'EACCES') {
-        logger(`Permission denied. Cannot bind to port ${port}.`, "[ Rudra ]");
+        logger(`Permission denied. Cannot bind to port ${port}.`, "[ MirryKal ]");
     } else {
-        logger(`Server error: ${err.message}`, "[ Rudra ]");
+        logger(`Server error: ${err.message}`, "[ Kripya Dhyan de ]");
     }
 });
 
@@ -35,7 +35,7 @@ app.listen(port, () => {
 global.countRestart = global.countRestart || 0;
 
 function startBot(message) {
-    if (message) logger(message, "[ Rudra ]");
+    if (message) logger(message, "[ MirryKal ]");
 
     const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "rudra.js"], {
         cwd: __dirname,
@@ -49,7 +49,7 @@ function startBot(message) {
             logger(`Bot exited with code ${codeExit}. Restarting... (${global.countRestart}/5)`, "[ Rudra ]");
             startBot();
         } else {
-            logger(`Bot stopped after ${global.countRestart} restarts.`, "[ Rudra ]");
+            logger(`Bot stopped after ${global.countRestart} restarts.`, "[ MirrKal]");
         }
     });
 
@@ -64,9 +64,9 @@ function startBot(message) {
 
 axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json")
     .then((res) => {
-        logger(res.data.name, "[ Rudra ]");
-        logger(`Version: ${res.data.version}`, "[ Rudra ]");
-        logger(res.data.description, "[ Rudra ]");
+        logger(res.data.name, "[ Mirrykal ]");
+        logger(`Version: ${res.data.version}`, "[ Mirrykal]");
+        logger(res.data.description, "[ Mirrykal ]");
     })
     .catch((err) => {
         logger(`Failed to fetch update info: ${err.message}`, "[ Rudra ]");
